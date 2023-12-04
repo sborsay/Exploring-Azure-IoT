e4d7ed2e66f363167024264c57713074e27f1660
Stephen Borsay <borsay@gmail.com>
2023-12-04 09:41:09 -0800

    Update Single_Payload_Send.cmd

--- Virtual-Devices/Single_Payload_Send.cmd
+++ Virtual-Devices/Single_Payload_Send.cmd
@@ -1,1 +1,25 @@
-sdg
+###Monitor
+---------
+```
+az iot hub monitor-events --hub-name <Your-IoT-Hub-Name>  --device-id <Your-IoT-Device-Name>
+```
+
+
+###OPTIONAL Formats (JSON is default)
+----------------------------------------
+```
+az iot hub monitor-events --hub-name <Your IoT Hub Name> --output json
+```
+
+```
+az iot hub monitor-events --hub-name iothubsdb1 --output table
+```
+-----------------------------------------
+
+
+###Send IoT Payload from a terminal (you can use escaped or unescaped payload variables)
+------------------------------------------------------------------------------------
+```
+az iot device send-d2c-message -n iothubsdb1 --device-id testdevice --data '{"temperature":73,"humidity":88, "timestamp":123456789}'
+```
+
