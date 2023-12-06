467b6b5f67400c7101e0b37c4f9c6b2326e656be
Stephen Borsay <borsay@gmail.com>
2023-12-06 10:13:18 -0800

    Create DataProducer.sh

---
+++ Virtual-Devices/DataProducer.sh
@@ -0,0 +1,30 @@
+#  Modified for Azure CLI by Stephen Borsay
+#  Original code found at: https://aws.amazon.com/blogs/iot/integrating-iot-data-with-your-data-lake-with-new-aws-iot-analytics-features/
+
+#!/bin/bash
+
+iterations=5
+wait=5
+
+for (( i = 1; i <= $iterations; i++)) {
+
+  CURRENT_TS=`date +%s`
+  TEMP=$(( 15 + $RANDOM % 20 ))
+  HUMIDITY=$(( 50 + $RANDOM % 40 ))
+
+  # 3% chance of throwing an anomalous temperature reading
+  if [ $(($RANDOM % 100)) -gt 97 ]
+  then
+    echo "Temperature out of range"
+    TEMP=$(($TEMP*6))
+  fi
+
+  echo "Publishing message $i/$ITERATIONS to IoT topic $mqtttopic:"
+  echo "temperature: $TEMP"
+  echo "humidity: $HUMIDITY"
+  echo "timestamp: $CURRENT_TS"
+
+ az iot device send-d2c-message -n iothubsdb1 -d testdevice --data "{"temperature":$TEMP,"humidity":$HUMIDITY, "timestamp":$CURRENT_TS}" 
+
+  sleep $wait
+}
